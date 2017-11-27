37efe6917d071b0d7ddbc92c44e156bb55efe623
NERVENERD\NerveNerd <dnsudhir@gmail.com>
2017-11-28 01:18:48 +0530

    Initial commit

---
+++ app/src/main/java/dnsudhir/com/odm/MainActivity.java
@@ -0,0 +1,15 @@
+package dnsudhir.com.odm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    
+
+  }
+}
